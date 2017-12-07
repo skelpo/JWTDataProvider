64aac85d07e6b1967c7e935ab736ef8d47bacb47
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-12-07 15:32:13 -0600

    Created Provider class with .boot(Config) method implimentation

---
+++ Sources/JWTDataProvider/JWTDataProvider.swift
@@ -0,0 +1,28 @@
+import Vapor
+
+internal private(set) var services: [Service] = []
+
+public final class Provider: Vapor.Provider {
+    public static var repositoryName: String = "JWTDataProvider"
+    
+    public func boot(_ config: Config) throws {
+        guard let service = config["service"] else {
+            throw ConfigError.missingFile("service.json")
+        }
+        guard let jsonServices = service["services"]?.array else {
+            throw ConfigError.missing(key: ["services"], file: "service.json", desiredType: Array<Service>.self)
+        }
+        services = try jsonServices.map { (json) -> Service in
+            guard let name = json["name"]?.string,
+                  let url = json["url"]?.string else {
+                    throw ConfigError.missing(key: ["name", "url"], file: "service.json", desiredType: String.self)
+            }
+            return Service(name: name, url: url)
+        }
+    }
+    
+    public func boot(_ droplet: Droplet) throws {}
+    public func beforeRun(_ droplet: Droplet) throws {}
+    public init(config: Config) throws {}
+}
+
