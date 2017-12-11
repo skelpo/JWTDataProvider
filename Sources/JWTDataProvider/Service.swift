897e57665f07b957ec4d0870818f7f5a24c743ca
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-12-11 07:37:59 -0600

    Added .body, .header, and .requiresAccessToken properties to Service struct

--- Sources/JWTDataProvider/Service.swift
+++ Sources/JWTDataProvider/Service.swift
@@ -1,7 +1,11 @@
 import HTTP
+import JSON
 
 internal struct Service {
     internal let name: String
     internal let url: String
     internal let method: Method
+    internal let body: JSON
+    internal var header: [HeaderKey: String]
+    internal let requiresAccessToken: Bool
 }
