a2fc3152ef0a9367dc54ef78c7a1b6b2556ab6a1
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-12-14 07:41:45 -0600

    Removed JSONFilter type

--- Sources/JWTDataProvider/Error.swift
+++
@@ -1,9 +0,0 @@
-public enum JWTDataProviderError: Error, CustomStringConvertible {
-    case badFilterKey(String)
-    
-    public var description: String {
-        switch self {
-        case let .badFilterKey(key): return "No key '\(key)' found in JSON"
-        }
-    }
-}
