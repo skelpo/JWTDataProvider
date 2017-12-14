589e432e22680e298c6884c2b7f7f1365185f55d
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-12-14 11:18:27 -0600

    Test for testing JSON

--- Tests/JWTDataProviderTests/JWTDataProviderTests.swift
+++ Tests/JWTDataProviderTests/JWTDataProviderTests.swift
@@ -1,12 +1,31 @@
 import XCTest
+import JSON
 @testable import JWTDataProvider
 
 class JWTDataProviderTests: XCTestCase {
     func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(JWTDataProvider().text, "Hello, World!")
+        let bytes = """
+        {
+            "foo": "bar"
+        }
+        """.makeBytes()
+        
+        let data2 = """
+        {
+            "click": 45
+        }
+        """.makeBytes()
+        
+        do {
+            let one = try JSON(bytes: bytes)
+            let two = try JSON(bytes: data2)
+            let json = try one.merge(two)
+            let serelized = try json.serialize(prettyPrint: true).makeString()
+            print(serelized)
+        } catch let error {
+            print("Error: ", error)
+            XCTFail()
+        }
     }
 
 
