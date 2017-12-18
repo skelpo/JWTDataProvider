3b7be3b1f0bc9ef90c5ad25d1da285dc2b998011
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-12-18 08:46:23 -0600

    Conformed JSONError to AbortError protocol

--- Sources/JWTDataProvider/Error.swift
+++ Sources/JWTDataProvider/Error.swift
@@ -1,13 +1,18 @@
-import JSON
+import Vapor
+import HTTP
 
-public enum JSONError: Error, CustomStringConvertible {
+public enum JSONError: Error, AbortError {
     case duplicateKey
     case badJSONStructure
     
-    public var description: String {
+    public var reason: String {
         switch self {
         case .duplicateKey: return "A duplicate key was found when merging JSON objects"
         case .badJSONStructure: return "Make sure you are adding the new keys to a JSON object, and not a value or array"
         }
+    }
+    
+    public var status: Status {
+        return .internalServerError
     }
 }
