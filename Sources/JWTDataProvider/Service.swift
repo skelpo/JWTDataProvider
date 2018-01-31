e401c219288f31d2f4fe56a4a4fb817854f77351
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-01-31 15:22:55 -0600

    Moved nested JSON data to ServiceData struct for proper decoding

--- Sources/JWTDataProvider/Service.swift
+++ Sources/JWTDataProvider/Service.swift
@@ -1,12 +1,16 @@
 import HTTP
 import JSON
 
-internal struct Service {
+internal struct Service: Decodable {
     let name: String
+    let data: ServiceData
+}
+
+internal struct ServiceData: Decodable {
     let url: String
-    let method: Method
+    let method: HTTPMethod
     let body: JSON
-    let header: [HeaderKey: String]
+    let header: [HTTPHeaderName: String]
     let requiresAccessToken: Bool
     let filter: String
     let `default`: Node
