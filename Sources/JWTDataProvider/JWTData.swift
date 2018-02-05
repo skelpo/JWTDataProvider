8e62d84cd3eb44d6e6a96aa83d5e750017562c75
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-02-05 08:04:59 -0600

    Changed JWTData.fetch to a Request extension method

--- Sources/JWTDataProvider/JWTData.swift
+++ Sources/JWTDataProvider/JWTData.swift
@@ -1,36 +1,74 @@
 import Vapor
 import JWT
 import HTTP
+import JSON
+import Foundation
 
-public final class JWTData {
-    public static func fetch(_ accessToken: String? = nil, parameters: [String: String])throws -> JSON {
-        var payload = JSON()
+extension Request {
+    public func fetch<Payload>(
+        _ accessToken: String? = nil,
+        with parameters: [String: String],
+        as payloadType: Payload.Type
+    )throws -> Future<Payload> where Payload: JWTPayload {
+        let result = Promise(Payload.self)
+        let client = try self.make(Client.self)
+        let serviceContainer = try self.make(DataServiceContainer.self)
         
-        try services.forEach({ service in
-            var header = service.header
-            let url = JWTData.replace(placeholders: parameters, in: service.url)
+        var responses: [Future<Response>] = []
+        var names: [String] = []
+        
+        try serviceContainer.services.forEach({ service in
+            var header = service.data.headers
+            let url = replace(placeholders: parameters, in: service.data.url)
             
-            if service.requiresAccessToken && accessToken != nil && header[.authorization] == nil {
-                header[.authorization] = "Bearer \(accessToken!)"
+            if service.data.requiresAccessToken {
+                if accessToken != nil && header[.authorization] == nil {
+                    header[.authorization] = "Bearer \(accessToken!)"
+                } else {
+                    result.fail(JSONError.noAccessToken(url))
+                    print("[JWTDataProvider - Bad API Usage: \(Date())] No access token passed in for service '\(service.name)' requiring access token.")
+                    return
+                }
             }
             
-            let response = try client.request(service.method, url, header, service.body)
-            if let json = response.json {
-                let value: Node = try json.get(service.filter)
-                try payload.set(service.name, value)
-            } else {
-                try payload.set(service.name, service.default)
-            }
+            let httpRequest = HTTPRequest(
+                method: service.data.method,
+                uri: URI(url),
+                headers: service.data.headers,
+                body: service.data.body
+            )
+            let request = Request(http: httpRequest, using: self.superContainer)
+            let response = try client.respond(to: request)
+            
+            responses.append(response)
+            names.append(service.name)
         })
         
-        return payload
-    }
-
-    fileprivate static func replace(placeholders: [String: String], `in` string: String) -> String {
-        var str = string
-        for (name, value) in placeholders {
-            str = str.replacingOccurrences(of: "{\(name)}", with: value)
+        return responses.flatten().flatMap(to: [JSON].self) { (reses)  in
+            return try reses.map({ (response) in
+                return try response.content.decode(JSON.self)
+            }).flatten()
+        }.map(to: JSON.self) { (objects) in
+            var data: [String: JSON] = [:]
+            
+            zip(names, objects).forEach({ (serviceResult) in
+                let (name, object) = serviceResult
+                data[name] = object
+            })
+            
+            return JSON.object(data)
+        }.flatMap(to: Payload.self) { (json) in
+            let data = try JSONEncoder().encode(json)
+            try result.complete(JSONDecoder().decode(Payload.self, from: data))
+            return result.future
         }
-        return str
     }
+}
+
+fileprivate func replace(placeholders: [String: String], `in` string: String) -> String {
+    var str = string
+    for (name, value) in placeholders {
+        str = str.replacingOccurrences(of: "{\(name)}", with: value)
+    }
+    return str
 }
