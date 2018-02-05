b9ef8b4190ef17eb1cd1f881d3e23b573e96c748
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-02-05 08:04:25 -0600

    Replaced global services and client variables with app services

--- Sources/JWTDataProvider/JWTDataProvider.swift
+++ Sources/JWTDataProvider/JWTDataProvider.swift
@@ -2,23 +2,25 @@ import Vapor
 import HTTP
 import Foundation
 
-internal private(set) var services: [Service] = []
-internal private(set) var client: ClientFactoryProtocol!
-
 public final class Provider: Vapor.Provider {
     public static var repositoryName: String = "JWTDataProvider"
 
     /// Register all services provided by the provider here.
-    public func register(_ services: inout Services) throws {}
+    public func register(_ services: inout Services) throws {
+        services.register(isSingleton: true) { (container) -> (DataServiceContainer) in
+            return DataServiceContainer()
+        }
+    }
 
     /// Called after the container has initialized.
     public func boot(_ worker: Container) throws {
+        let container = try worker.make(DataServiceContainer.self, for: Container.self)
         let file = File(on: worker)
         let decoder = JSONDecoder()
         let root = DirectoryConfig.detect().workDir
         
-        file.read(at: root + "/Config/services.json", chunkSize: 16384).map(to: Services.self) { (data) in
-            services = decoder.decode(Services.self, from: data).all
+        _ = file.read(at: root + "/Config/services.json", chunkSize: 16384).map(to: Void.self) { (data) in
+            container.services = try decoder.decode(DataServices.self, from: data).all
         }
     }
 }
