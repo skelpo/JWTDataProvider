9234f069db575dcf496645e432963a7b5a112646
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-02-12 15:12:59 -0600

    Changed JSONError enum to JWTDataError struct

--- Sources/JWTDataProvider/Error.swift
+++ Sources/JWTDataProvider/Error.swift
@@ -1,22 +1,18 @@
 import Vapor
-import HTTP
+import Debugging
 
-public enum JSONError: Error, AbortError {
-    case noAccessToken(String)
-    
-    public var identifier: String {
-        switch self {
-        case .noAccessToken: return "noAccessToken"
-        }
-    }
-    
-    public var reason: String {
-        switch self {
-        case let .noAccessToken(url): return "No access token was found for service accesing enpoint '\(url)'. Did you forget to pass one in?"
-        }
-    }
-    
-    public var status: HTTPStatus {
-        return .internalServerError
+public struct JWTDataError: Debuggable, AbortError {
+    public let identifier: String
+    public let reason: String
+    public let status: HTTPStatus
+}
+
+extension JWTDataError {
+    public static func noAccessToken(_ url: String) -> JWTDataError {
+        return self.init(
+            identifier: "noAccessToken",
+            reason: "No access token was found for service accesing enpoint '\(url)'. Did you forget to pass one in?",
+            status: .internalServerError
+        )
     }
 }
