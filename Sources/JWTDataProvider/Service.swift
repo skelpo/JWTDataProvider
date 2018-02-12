dc10816b8d8ee23b5e4023aa5ee9a7c7a95de489
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-02-12 12:17:35 -0600

    Replaced DataService structs with protocols

--- Sources/JWTDataProvider/Service.swift
+++ Sources/JWTDataProvider/Service.swift
@@ -1,46 +1,21 @@
+import Service
 import HTTP
-import Vapor
-import JSON
+@_exported import JSON
 
-final internal class DataServiceContainer: Service {
-    var services: [DataService] = []
+public protocol DataServices: Service {
+    var services: [String: DataService] { get }
     
-    init() {}
+    init()
 }
 
-internal struct DataServices: Decodable {
-    let services: [String: ServiceData]
-
-    var all: [DataService] {
-        return self.services.map { (name, data) in
-            return DataService(name: name, data: data)
-        }
-    }
-}
-
-internal struct DataService: Decodable {
-    let name: String
-    let data: ServiceData
-}
-
-internal struct ServiceData: Codable {
-    let url: String
-    let method: HTTPMethod
-    let body: HTTPBody
-    let headers: HTTPHeaders
-    let requiresAccessToken: Bool
-    let filter: String
-    let `default`: JSON?
+public protocol DataService: Service {
+    var url: String { get }
+    var method: HTTPMethod { get }
+    var body: HTTPBody { get }
+    var headers: HTTPHeaders { get }
+    var requiresAccessToken: Bool { get }
+    var jsonPath: [String] { get }
+    var `default`: JSON { get }
     
-    init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: ServiceData.CodingKeys.self)
-        
-        self.url = try container.decode(String.self, forKey: .url)
-        self.method = try container.decodeIfPresent(HTTPMethod.self, forKey: .method) ?? .get
-        self.body = try container.decodeIfPresent(HTTPBody.self, forKey: .body) ?? HTTPBody()
-        self.headers = try container.decodeIfPresent(HTTPHeaders.self, forKey: .headers) ?? [:]
-        self.requiresAccessToken = try container.decodeIfPresent(Bool.self, forKey: .requiresAccessToken) ?? false
-        self.filter = try (container.decodeIfPresent(Array<String>.self, forKey: .filter) ?? []).joined(separator: ".")
-        self.default = try container.decodeIfPresent(JSON.self, forKey: .default) ?? .null
-    }
+    init()
 }
