8a41bbd91043567ee1b0b8c79d9468816dbb8804
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-02-13 15:07:26 -0600

    Set dependancy versions to tags

--- Package.swift
+++ Package.swift
@@ -6,9 +6,9 @@ let package = Package(
         .library(name: "JWTDataProvider", targets: ["JWTDataProvider"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/vapor/vapor.git", .branch("beta")),
-        .package(url: "https://github.com/vapor/jwt.git", .branch("beta")),
-        .package(url: "git@github.com:skelpo/JSON.git", .branch("master"))
+        .package(url: "https://github.com/vapor/vapor.git", .exact("3.0.0-beta.2")),
+        .package(url: "https://github.com/vapor/jwt.git", .exact("3.0.0-beta.1")),
+        .package(url: "git@github.com:skelpo/JSON.git", .exact("0.6.0"))
     ],
     targets: [
         .target(name: "JWTDataProvider", dependencies: ["Vapor", "JWT", "JSONKit"]),
