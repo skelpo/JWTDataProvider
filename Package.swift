3bfe86a672fc8e1eb7a57639ad4dd7fba9692753
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-02-28 09:15:08 -0600

    Updated vapor dependencies to RC releases

--- Package.swift
+++ Package.swift
@@ -6,9 +6,9 @@ let package = Package(
         .library(name: "JWTDataProvider", targets: ["JWTDataProvider"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0-beta.3.1.2"),
-        .package(url: "https://github.com/vapor/jwt.git", from: "3.0.0-beta.1.1"),
-        .package(url: "git@github.com:skelpo/JSON.git", from: "0.7.2")
+        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0-rc"),
+        .package(url: "https://github.com/vapor/jwt.git", from: "3.0.0-rc"),
+        .package(url: "git@github.com:skelpo/JSON.git", from: "0.10.0")
     ],
     targets: [
         .target(name: "JWTDataProvider", dependencies: ["Vapor", "JWT", "JSONKit"]),
