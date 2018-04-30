233848cc471944093eb397a465581784d303dd5c
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-04-30 06:32:49 -0500

    Changed DataService type from a protocol to a struct

--- Sources/JWTDataProvider/Service.swift
+++ Sources/JWTDataProvider/Service.swift
@@ -3,38 +3,22 @@ import Vapor
 
 extension Data: Content {}
 
-public protocol DataService: Service {
-    var url: String { get }
-    var method: HTTPMethod { get }
-    var body: Data { get }
-    var headers: HTTPHeaders { get }
-    var requiresAccessToken: Bool { get }
-    var jsonPath: [String] { get }
-    var `default`: JSON { get }
-}
-
-extension DataService {
-    public var method: HTTPMethod {
-        return .GET
-    }
+public struct DataService: Service {
+    var url: String
+    var method: HTTPMethod
+    var body: Data
+    var headers: HTTPHeaders
+    var requiresAccessToken: Bool
+    var jsonPath: [String]
+    var `default`: JSON
     
-    public var body: Data {
-        return Data()
-    }
-    
-    public var headers: HTTPHeaders {
-        return [:]
-    }
-    
-    public var requiresAccessToken: Bool {
-        return false
-    }
-    
-    public var jsonPath: [String] {
-        return []
-    }
-    
-    public var `default`: JSON {
-        return nil
+    init(url: String, method: HTTPMethod = .GET, body: Data = Data(), headers: HTTPHeaders = [:], requiresAccessToken: Bool = false, jsonPath: [String] = [], default: JSON = nil) {
+        self.url = url
+        self.method = method
+        self.body = body
+        self.headers = headers
+        self.requiresAccessToken = requiresAccessToken
+        self.jsonPath = jsonPath
+        self.default = `default`
     }
 }
