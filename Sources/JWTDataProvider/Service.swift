3d90eb7c38566e349f76c19df4aa58be1e3ed45b
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-04-30 09:33:47 -0500

    Changed RemoteDataClient.body property type to JSON

--- Sources/JWTDataProvider/Service.swift
+++ Sources/JWTDataProvider/Service.swift
@@ -1,18 +1,16 @@
 import Vapor
 @_exported import JSONKit
 
-extension Data: Content {}
-
 public struct RemoteDataClient: Service {
     public var url: String
     public var method: HTTPMethod
-    public var body: Data
+    public var body: JSON
     public var headers: HTTPHeaders
     public var requiresAccessToken: Bool
     public var jsonPath: [String]
     public var `default`: JSON
     
-    public init(url: String, method: HTTPMethod = .GET, body: Data = Data(), headers: HTTPHeaders = [:], requiresAccessToken: Bool = false, jsonPath: [String] = [], default: JSON = nil) {
+    public init(url: String, method: HTTPMethod = .GET, body: JSON = [:], headers: HTTPHeaders = [:], requiresAccessToken: Bool = false, jsonPath: [String] = [], default: JSON = nil) {
         self.url = url
         self.method = method
         self.body = body
