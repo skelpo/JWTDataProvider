935cc20894ce9edba216f7f9f47e46b6b91c26a7
Ralph Küpper <ralph.kuepper@skelpo.com>
2018-05-09 10:26:01 -0400

    Updated Package.swift to load from https

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
         .package(url: "https://github.com/vapor/jwt.git", from: "3.0.0-rc"),
-        .package(url: "git@github.com:skelpo/JSON.git", from: "0.13.0")
+        .package(url: "https://github.com/skelpo/JSON.git", from: "0.13.0")
     ],
     targets: [
         .target(name: "JWTDataProvider", dependencies: ["Vapor", "JWT", "JSONKit"]),
